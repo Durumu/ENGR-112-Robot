f942aab7b89ea535fd17775b3ad2a509ab80abe6
Jack Graham <jackpg98@gmail.com>
2017-04-15 16:37:47 -0500

    changed color_reader

--- motor_to_rotation.m
+++ motor_to_rotation.m
@@ -1,8 +1,4 @@
 function motor_to_rotation(motor,target,speed,time,acc)
-if nargin == 2
-    speed = 2;
-    time = 0.05;
-end
 if nargin < 5
     acc = 2;
 end
@@ -10,8 +6,12 @@ end
 current_point = readRotation(motor);
 while abs(current_point-target) >= acc
     s = speed;
-    if abs(current_point-target) >= acc * 10
-        s = min(100,s*3);
+    if abs(current_point-target) >= 5*acc + 15
+        if s > 0
+            s = max(s*5,100);
+        else
+            s = min(s*5,-100);
+        end
     end
     
     if target < current_point
